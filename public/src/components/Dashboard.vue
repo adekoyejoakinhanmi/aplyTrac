9f142d51ed38b640e50ec53fd0d8496408561e42
adekoyejoakinhanmi <koyesavedbygrace@yahoo.com>
2017-09-25 12:54:21 +0100

    componentalized appForm

--- public/src/components/Dashboard.vue
+++ public/src/components/Dashboard.vue
@@ -1,70 +1,22 @@
 <template>
   <div>
-    <div :class="['row', 'mb-1', formVisible ? '' : 'hidden']">
-       <form>
-            <div class="col-md-3 mb-1">
-                <label for="Company Name">Company Name</label>
-                <input class="form-control" v-model="newApp.companyName" type="text" name="Company Name" placeholder="Company Name">
-            </div>
-            <div class="col-md-3 mb-1">
-               <label for="Job Vacancy">Job Vacancy</label>
-               <select class="form-control" v-model="newApp.jobVacancy" name="Job Vacancy">
-                  <option value="Web Developer">Web Developer</option>
-                  <option value="Fron End Developer">Front End Developer</option>
-                  <option value="JavaScript Developer">JavaScript Developer</option>
-                  <option value="Full Stack Developer">Full Stack Developer</option>
-                  <option value="Other">Other</option>
-               </select>
-            </div>
-            <div class="col-md-3 mb-1">
-                  <label for="Job Type">Job Type</label>
-                  <select class="form-control" v-model="newApp.jobType"  name="Job Type">
-                     <option value="Remote / Telecommute">Remote / Telecommute</option>
-                     <option value="On Site">On Site</option>
-                  </select>
-            </div>
-            <div class="col-md-3 mb-1">
-                  <label for="Application Medium">Application Medium</label>
-                  <select class="form-control" v-model="newApp.applicationMedium" name="Application Medium">
-                     <option value="Email">Email</option>
-                     <option value="Remotee">Remotee</option>
-                     <option value="Workable">Workable</option>
-                     <option value="Lever.co">Lever.co</option>
-                     <option value="Other">Other</option>
-                  </select>
-            </div>
-            <div class="col-md-3 mb-1">
-                  <label for="Application Status">Application Status</label>
-                  <select class="form-control" v-model="newApp.applicationStatus" name="Application Status">
-                     <option value="Yet to reply">Yet to reply</option>
-                     <option value="Pending">Pending</option>
-                     <option value="Get Back to you">Get Back to you</option>
-                     <option value="Rejected/Filled">Rejected/Filled</option>
-                  </select>
-            </div>
-            <div class="col-md-3 mb-1">
-                  <label for="Date Applied">Application Date</label>
-                  <input class="form-control" v-model="newApp.applicationDate" name="Date Applied" type="date">
-            </div>
-            <div class="pull-right">
-               <button id="cancelNewApplication" class="btn btn-default" type="reset">Cancel</button>
-               <button class="btn btn-success" type="button" @click="addNew" :disabled="formValid">Add</button>
-            </div>
-      </form>
-    </div>
+    
 
      <header class="row mb-1">
       <div class="col-md-10 col-sm-7 col-xs-7">
         <h2>All Jobs for {{user}}</h2>
       </div>
       <div class="col-md-2 col-sm-5 col-xs-5 clearfix">
-        <button @click="toggleFormVisiblity" id="addNewApplication" class="pull-right btn btn-success">
+        <button @click="toggleFormVisibility" id="addNewApplication" class="pull-right btn btn-success">
          Add New <i class="glyphicon glyphicon-plus"></i>
         </button>
       </div>
       </header>
    
-    
+    <app-form :form-visible="formVisible"
+              v-on:newApp="addNewApp"
+              v-on:formClosed="toggleFormVisibility"
+    ></app-form>
 
     <div class="row">
       
@@ -82,24 +34,17 @@
 </template>
 
 <script>
-import axios from 'axios';
-import application from '../reusable/application.vue';
+import axios from 'axios';;
 import base from '../helpers/urls.config';
-import uniqid from 'uniqid';
 import _ from 'lodash';
+
+import application from '../reusable/application.vue';
+import appForm from '../reusable/appForm.vue';
 
 export default {
    data(){
       return {
          user : "Adekoyejo",
-         newApp : {
-            companyName : '',
-            jobVacancy : 'Web Developer',
-            jobType : 'Remote / Telecommute',
-            applicationMedium : 'Email',
-            applicationStatus : 'Yet to reply',
-            applicationDate : ''
-         },
          formVisible : false,
          applications : []
       }
@@ -120,40 +65,20 @@ export default {
        this.applications.splice(idx, 1, other);
        return true;
      },
-     addNew() {
-       let id = `${Math.floor(Math.random() * 10)}${uniqid()}${uniqid()}${uniqid()}`;
-
-       let data = {
-         id : id.substring(0,19),
-         companyName : this.newApp.companyName,
-         jobVacancy : this.newApp.jobVacancy,
-         jobType : this.newApp.jobType,
-         applicationMedium : this.newApp.applicationMedium,
-         applicationStatus : this.newApp.applicationStatus,
-         applicationDate : this.newApp.applicationDate
-       }
-       axios.post(`${base.url}/applications`, data).then(success => {
-         this.applications.unshift(data);
-       }).catch(err => {
-         console.log(err);
-       });
-       this.newApp.companyName = '';
+     addNewApp(newData) {
+       this.applications.unshift(newData);
      },
-     toggleFormVisiblity(){
+     toggleFormVisibility(){
        this.formVisible = !this.formVisible;
        return true; 
-     }
-   },
-   computed : {
-     formValid() {
-       return !this.newApp.companyName && !this.newApp.applicationDate;
      }
    },
    mounted() {
      this.getApplications();
    },
    components : {
-     'application' : application
+     'application' : application,
+     'appForm' : appForm
    }
 }
 </script>
---
+++ public/src/reusable/appForm.vue
@@ -0,0 +1,108 @@
+<template>
+      <div :class="['row', 'mb-1', formVisible ? '' : 'hidden']">
+       <form>
+            <div class="col-md-3 mb-1 col-md-6">
+                <label for="Company Name">Company Name</label>
+                <input class="form-control" v-model="newApp.companyName" type="text" name="Company Name" placeholder="Company Name">
+            </div>
+            <div class="col-md-3 mb-1 col-md-6">
+               <label for="Job Vacancy">Job Vacancy</label>
+               <select class="form-control" v-model="newApp.jobVacancy" name="Job Vacancy">
+                  <option value="Web Developer">Web Developer</option>
+                  <option value="Fron End Developer">Front End Developer</option>
+                  <option value="JavaScript Developer">JavaScript Developer</option>
+                  <option value="Full Stack Developer">Full Stack Developer</option>
+                  <option value="Other">Other</option>
+               </select>
+            </div>
+            <div class="col-md-3 mb-1 col-md-6">
+                  <label for="Job Type">Job Type</label>
+                  <select class="form-control" v-model="newApp.jobType"  name="Job Type">
+                     <option value="Remote / Telecommute">Remote / Telecommute</option>
+                     <option value="On Site">On Site</option>
+                  </select>
+            </div>
+            <div class="col-md-3 mb-1 col-md-6">
+                  <label for="Application Medium">Application Medium</label>
+                  <select class="form-control" v-model="newApp.applicationMedium" name="Application Medium">
+                     <option value="Email">Email</option>
+                     <option value="Remotee">Remotee</option>
+                     <option value="Workable">Workable</option>
+                     <option value="Lever.co">Lever.co</option>
+                     <option value="Other">Other</option>
+                  </select>
+            </div>
+            <div class="col-md-3 mb-1 col-md-6">
+                  <label for="Application Status">Application Status</label>
+                  <select class="form-control" v-model="newApp.applicationStatus" name="Application Status">
+                     <option value="Yet to reply">Yet to reply</option>
+                     <option value="Pending">Pending</option>
+                     <option value="Get Back to you">Get Back to you</option>
+                     <option value="Rejected/Filled">Rejected/Filled</option>
+                  </select>
+            </div>
+            <div class="col-md-3 mb-1 col-md-6">
+                  <label for="Date Applied">Application Date</label>
+                  <input class="form-control" v-model="newApp.applicationDate" name="Date Applied" type="date">
+            </div>
+            <div class="pull-right">
+               <button id="cancelNewApplication" class="btn btn-default" type="reset" @click="closeForm">Cancel</button>
+               <button class="btn btn-success" type="button" @click="addNew" :disabled="formValid">Add</button>
+            </div>
+      </form>
+    </div>
+</template>
+
+<script>
+import uniqid from 'uniqid';
+import axios from 'axios';
+import base from '../helpers/urls.config';
+
+export default {
+      props : ['formVisible'],
+      data() {
+            return {
+                  newApp : {
+                        companyName : '',
+                        jobVacancy : 'Web Developer',
+                        jobType : 'Remote / Telecommute',
+                        applicationMedium : 'Email',
+                        applicationStatus : 'Yet to reply',
+                        applicationDate : ''
+                  }
+            }
+      },
+      methods : {
+            addNew() {
+                  let _id = `${Math.floor(Math.random() * 10)}${uniqid()}${uniqid()}${uniqid()}`;
+
+                  let data = {
+                        id : _id.substring(0,19),
+                        companyName : this.newApp.companyName,
+                        jobVacancy : this.newApp.jobVacancy,
+                        jobType : this.newApp.jobType,
+                        applicationMedium : this.newApp.applicationMedium,
+                        applicationStatus : this.newApp.applicationStatus,
+                        applicationDate : this.newApp.applicationDate
+                  }
+                  axios.post(`${base.url}/applications`, data).then(success => {
+                        this.$emit('newApp', data);
+                  }).catch(err => {
+                        console.log(err);
+                  });
+            },
+            closeForm() {
+                  this.$emit('formClosed');
+            }
+      },
+      computed : {
+            formValid() {
+                  return !this.newApp.companyName && !this.newApp.applicationDate;
+            }
+      }
+}
+</script>
+
+<style>
+
+</style>
